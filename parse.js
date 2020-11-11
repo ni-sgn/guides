186b0e5007a4cb4b685ef9332ddcaed33df72299
ni-sgn <ni.saganeli@gmail.com>
2020-11-11 21:11:56 +0400

    .

---
+++ parse.js
@@ -0,0 +1,24 @@
+var myArray = {"row_0" : document.getElementById('row_0').innerHTML,
+	"row_1" : document.getElementById('row_1').innerHTML,
+	"row_2" : document.getElementById('row_2').innerHTML,
+	"row_3" : document.getElementById('row_3').innerHTML,
+	"row_4" : document.getElementById('row_4').innerHTML,
+	"row_5" : document.getElementById('row_5').innerHTML,
+	"row_6" : document.getElementById('row_6').innerHTML}
+
+var orshabati, samshabati, otxshabati, xutshabati, paraskevi
+for(var key in myArray)
+{
+	if (myArray[key].includes('????????')
+		orshabati.push(myArray[key])
+	if (myArray[key].includes('????????')
+		samshabati.push(myArray[key])
+	if (myArray[key].includes('????????')
+		otxshabati.push(myArray[key])
+	if (myArray[key].includes('????????')
+		xutshabati.push(myArray[key])
+	if (myArray[key].includes('????????')
+		paraskevi.push(myArray[key])
+}
+	
+
